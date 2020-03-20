a68f1362cd36de765c53b4bd654986fd76ed10c9
dairun <1187118529@qq.com>
2020-03-20 11:57:19 +0800

    注释

--- models/music.js
+++ models/music.js
@@ -1,10 +1,12 @@
 const db = require('./db.js')
 
 module.exports = {
+  // 添加音乐
   addMusicByObj: async(sing) => {
     let sings = await db.q('insert into music (title, singer, time, filelrc, file, uid) values (?,?,?,?,?,?)', Object.values(sing))
     return sings
   },
+  // 编辑音乐
   updateMusic: async(music) => {
     let musics = await db.q('update music set title=?, singer=?, time=?, filelrc=?, file=?, uid=? where id=?',Object.values(music))
     return musics
